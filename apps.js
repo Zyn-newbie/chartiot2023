890c86b76470709d2702096948e15c2de3be1d8f
handipradana <113164329+handipradana@users.noreply.github.com>
2023-08-30 10:40:57 +0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -7,14 +7,15 @@ const app = express();
 const port = 3000;
 
 const awsConfig = {
-  region: 'us-east-1',
+  region: 'us-east-1',  
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    accessKeyId: process.env.AWS_ACCESS_KEY,    
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     sessionToken: process.env.AWS_SESSION_TOKEN
   }
 };
 
+console.log("AWS_ACCESS_KEY: ", process.env.AWS_ACCESS_KEY)
 const dynamoDBClient = new DynamoDBClient(awsConfig);
 const tableName = 'sensor';
 
@@ -29,12 +30,24 @@ app.get('/', async (req, res) => {
       const responseData = data.Items.map(item => ({
         timestamp: item.timestamp.S,
         humidity: parseFloat(item.humidity.N),
-        temperature: parseFloat(item.temperature.N)
+        temperature: parseFloat(item.temperature.N),
+        accel_x: parseFloat(item.accel_x.N),
+        accel_y: parseFloat(item.accel_y.N),
+        accel_z: parseFloat(item.accel_z.N),
+        gyro_x: parseFloat(item.gyro_x.N),
+        gyro_y: parseFloat(item.gyro_y.N),
+        gyro_z: parseFloat(item.gyro_z.N)
       }));
 
       const labels = responseData.map(item => new Date(parseInt(item.timestamp) * 1000).toLocaleTimeString());
       const humidityValues = responseData.map(item => item.humidity);
       const temperatureValues = responseData.map(item => item.temperature);
+      const accel_xValues = responseData.map(item => item.accel_x);
+      const accel_yValues = responseData.map(item => item.accel_y);
+      const accel_zValues = responseData.map(item => item.accel_z);
+      const gyro_xValues = responseData.map(item => item.gyro_x);
+      const gyro_yValues = responseData.map(item => item.gyro_y);
+      const gyro_zValues = responseData.map(item => item.gyro_z);
 
       const html = `
         <!DOCTYPE html>
@@ -63,7 +76,7 @@ app.get('/', async (req, res) => {
         </head>
         <body>
           
-          <div class="container mt-5">
+          <div class="container mt-1">
           <h2 style="text-align:center"> IoT Monitoring </h2>
             <div class="row">
               <div class="col-md-6">
@@ -72,7 +85,29 @@ app.get('/', async (req, res) => {
               <div class="col-md-6">
                 <div id="temperature-chart" class="p-3"></div>
               </div>
-            </div>            
+            </div>    
+            <div class="row">
+              <div class="col-md-4">
+                <div id="accel-x-chart" class="p-3"></div>
+              </div>
+              <div class="col-md-4">
+                <div id="accel-y-chart" class="p-3"></div>
+              </div>
+              <div class="col-md-4">
+                <div id="accel-z-chart" class="p-3"></div>
+              </div>
+            </div>         
+            <div class="row">
+              <div class="col-md-4">
+                <div id="gyro-x-chart" class="p-3"></div>
+              </div>
+              <div class="col-md-4">
+                <div id="gyro-y-chart" class="p-3"></div>
+              </div>
+              <div class="col-md-4">
+                <div id="gyro-z-chart" class="p-3"></div>
+              </div>
+            </div>       
             <h3 style="text-align:center"> LKSN 2023 - Cloud Computing IoT Project</h3>
           </div>
           <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
@@ -80,6 +115,12 @@ app.get('/', async (req, res) => {
             const labels = ${JSON.stringify(labels)};
             const humidityValues = ${JSON.stringify(humidityValues)};
             const temperatureValues = ${JSON.stringify(temperatureValues)};
+            const accel_xValues = ${JSON.stringify(accel_xValues)};
+            const accel_yValues = ${JSON.stringify(accel_yValues)};
+            const accel_zValues = ${JSON.stringify(accel_zValues)};
+            const gyro_xValues = ${JSON.stringify(gyro_xValues)};
+            const gyro_yValues = ${JSON.stringify(gyro_yValues)};
+            const gyro_zValues = ${JSON.stringify(gyro_zValues)};
 
             // Create humidity indicator chart using Plotly
             var humidityIndicator = {
@@ -133,6 +174,35 @@ app.get('/', async (req, res) => {
             };
 
             Plotly.newPlot('temperature-chart', [temperatureIndicator], temperatureIndicatorLayout);
+
+            function createLineChart(elementId, title, xValues, yValues) {
+              var trace = {
+                x: xValues,
+                y: yValues,
+                mode: 'lines',
+                type: 'scatter',
+                name: title
+              };
+          
+              var layout = {
+                title: title,
+                xaxis: {
+                  title: 'Timestamp'
+                },
+                yaxis: {
+                  title: title
+                }
+              };
+          
+              Plotly.newPlot(elementId, [trace], layout);
+            }
+          
+            createLineChart('accel-x-chart', 'Accel X', labels, accel_xValues);
+            createLineChart('accel-y-chart', 'Accel Y', labels, accel_yValues);
+            createLineChart('accel-z-chart', 'Accel Z', labels, accel_zValues);
+            createLineChart('gyro-x-chart', 'Gyro X', labels, gyro_xValues);
+            createLineChart('gyro-y-chart', 'Gyro Y', labels, gyro_yValues);
+            createLineChart('gyro-z-chart', 'Gyro Z', labels, gyro_zValues);
           </script>
 
         </body>
